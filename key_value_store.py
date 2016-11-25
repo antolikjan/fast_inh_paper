dc056a2668563b84dbbb4258d248ef8c6958b798
antolikjan <antolikjan@gmail.com>
2016-11-25 10:04:31 +0100

    changes

---
+++ key_value_store.py
@@ -0,0 +1,29 @@
+{
+	# ARCHITECTURE
+	'cortex_size' : ?,
+	'lgn_size' : ?,
+	'visual_field_size' : ?,
+	'cortex_density' : ?,
+	'settle_steps' : ?,
+	'num_gaussian_stim' : ?,
+
+
+	# LGN
+	'retina_to_lgn_strength' : ?,
+	'lateral_lgn_strength' : ?,
+	'lateral_lgn_gain' : ?,
+	'lgn_center_sigma' : ?,
+	'lgn_surround_sigma' : ?,
+	'lgn_lateral_sigma' : ?,
+
+	# Homeostasis
+	'homeostatic_learning_rate'  : ?,
+	'target_activity' : ?,
+	'activity_average_smoothing' : ?,
+	
+	# Hebbian adaptation
+	'learning_rate' : ?,
+
+
+
+}
