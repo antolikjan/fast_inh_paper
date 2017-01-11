6fd08b98d94a4ba6b419339342603e8d1b36e148
Jan Antolik <antolikjan@gmail.com>
2017-01-11 12:29:12 +0100

    added tifs

--- build_figures.sh
+++ build_figures.sh
@@ -6,4 +6,5 @@ for x in ./SVG/Figure1/figure1.svg  ./SVG/Figure2/figure2.svg ./SVG/Figure3/figu
   base=`basename $x .svg`
   pwd
   cd ${dir}; inkscape ${base}.svg --export-area-drawing --export-dpi=600 -e ${base}.png; cd ../../
+  cd ${dir}; inkscape ${base}.svg --export-area-drawing --export-dpi=600 -e ${base}.tif; cd ../../
 done
